5d63f606dfdbd0e66cfcbf8e770e0794c71abc2d
rifqiraehan <rifqiraehan86@gmail.com>
2023-08-31 21:36:39 +0700

    initial changes

--- Praktikum 2/Percobaan 2/EXPRESS5.c
+++ Praktikum 2/Percobaan 2/EXPRESS5.c
@@ -5,16 +5,16 @@ int main(){
     int b1, b2 = -150;
 
     b1 = p1; // Konversi Pecahan ke Bulat
-    printf("%f disimpan ke int menghasilkan %d\n\n", p1, b1);
+    printf("%f disimpan ke int menghasilkan %d\n", p1, b1);
 
     p1 = b2; // Konversi Bulat ke Pecahan
-    printf("%d disimpan ke float menghasilkan %f\n\n", b2, p1);
+    printf("%d disimpan ke float menghasilkan %f\n", b2, p1);
 
     p1 = b2 / 100; // Bulat dibagi Bulat
-    printf("%d dibagi 100 menghasilkan %fd\n\n", b2, p1);
+    printf("%d dibagi 100 menghasilkan %f\n", b2, p1);
 
     p2 = b2 / 100.0; // Bulat dibagi Pecahan
-    printf("%d dibagi 100 menghasilkan %f\n\n", b2, p2);
+    printf("%d dibagi 100 menghasilkan %f\n", b2, p2);
 
     return 0;
 }
