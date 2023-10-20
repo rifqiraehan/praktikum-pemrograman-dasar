80411b9f6c772a764ff32a4b932b39784538adbb
rifqiraehan <rifqiraehan86@gmail.com>
2023-10-20 10:21:00 +0700

    menambahkan praktikum 7

---
+++ Praktikum 7/Tugas 7/Tugas_751.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int i, j, a, n, data[5];
+    printf("Masukkan jumlah data yang dimasukkan: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Masukkan data ke-%d: ", i + 1);
+        scanf("%d", &data[i]);
+    }
+
+    printf("\nList data dengan urutan Ascending sebagai berikut:\n");
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (data[i] > data[j]) {
+                a = data[i];
+                data[i] = data[j];
+                data[j] = a;
+            }
+
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d\n", data[i]);
+    }
+
+    return 0;
+}
