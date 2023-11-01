a0f99f9122bda5b4e5651444c3976f22c29ef899
rifqiraehan <rifqiraehan86@gmail.com>
2023-11-01 19:38:10 +0700

    revisi

--- Praktikum 8/Tugas 8/Tugas_854.c
+++ Praktikum 8/Tugas 8/Tugas_854.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
+void gotoxy(int x, int y) {
+    COORD curpos;
+    HANDLE hConsoleOut;
+    curpos.X = x;
+    curpos.Y = y;
 
-int main(){
+    hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsoleOut, curpos);
+}
 
+int main() {
+    int x, y, i, j;
     char str[100] = "POLITEKNIK ELEKTRONIKA NEGERI SURABAYA";
-    int i, j;
 
     for(i = 0; i < 38; i++){
+        x = strlen(str) - i;
+        y = i + 1;
         for(j = 0; j <= i; j++){
+            gotoxy(x, y);
             printf("%c", str[j]);
+            x++;
         }
-        printf("\n");
     }
 
     return 0;
